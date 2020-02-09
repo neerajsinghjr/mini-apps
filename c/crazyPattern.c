a198be61bc269bf11621f9d24b7ceedfb405d50f
NeerajSingh511997 <NeerajSingh511997@gmail.com>
2020-02-09 13:57:54 +0530

    added new working modules

---
+++ c/crazyPattern.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+
+#define MAX 100;
+
+int main() {
+    /**
+     * Print the following Pattern;
+     * a(1) = 1,
+     * a(2) = 1,
+     * a(n) = a(a(n-1)) + a(n-a(n-1))
+     */
+
+    int i, n, 
+    printf("Limit Of Size N: ");
+    scanf("%d", )
+    for(int i=1; i<)
+}
---
+++ php/basic.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Basic PHP</title>
+	<link href="css/basic.css" rel="stylesheet" />
+</head>
+<body>
+
+<?php
+	/*	checking out echo and print */
+	$first = "neeraj";
+	$middle = "singh";
+	$last = "junior";
+	echo($first, $middle, $junior);
+	echo $first, $middle, $junior;
+
+?>
+
+</body>
+</html>
