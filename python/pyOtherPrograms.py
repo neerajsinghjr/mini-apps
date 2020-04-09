8a7134f80897ea54120bfc1538e17f524a9599fd
Junior <NeerajSingh511997@gmail.com>
2020-04-09 10:13:16 +0530

    updated pyModules

--- python/pyOtherPrograms.py
+++ python/pyOtherPrograms.py
@@ -510,3 +510,15 @@
 
 
 ############ 08 April 2020;
+
+## Program: String inbuilt 5 functions;
+try:
+    string = input("Your String: ")
+    print("1. Convert To Lowercase: %s" %(string.lower()))
+    print("2. Convert To Uppercase: %s" %(string.upper()))
+    print("3. String Split Func: %s" %(string.split(" ")))
+    print("4. Check if Lowercase: %s" %(string.islower()))
+    print("5. Check if uppercase: %s" %(string.isupper()))
+    
+except Exception as error:
+    print("Exception Occured.", error)
